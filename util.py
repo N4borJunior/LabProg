146264dad311aff1c74e8f01dac2abb093083a14
N4borJunior <juniornabor58@gmail.com>
2026-03-23 10:21:33 -0400

    Update util.py

--- util.py
+++ util.py
@@ -1,16 +1,19 @@
+
 def inputint(msg="Digite um valor inteiro: ",min=None,max=None):
+    erro = True
+    while erro == True:
     try:
         valor = int(input(msg))
         if min!=None and valor < min:
             raise Exception(f'ERRO: valor é menor do que o mínimo permitido de {min}')
         if max!=None and valor > max:
             raise Exception(f'ERRO: valor é maior do que o máximo permitido de {max}')
+        erro = False
         return valor
     except ValueError:
         print ('ERRO: Valor informado não é inteiro!')
     except Exception as e:
         print(e)
-    return -1
 
 def inputfloat(msg="Digite um número real: ",min=None,max=None):
     try:
